23fa7ff8c7645bc850a6bed62b34987c06464699
Sergey Litvinov <slitvinov@gmail.com>
2017-04-23 17:04:24 +0200

    Update bop2vtk

--- octave/bop2vtk.m
+++ octave/bop2vtk.m
@@ -1,16 +1,24 @@
-% bop to vtk converter
+#!/usr/bin/env octave-qf
 
-fn  = '../test_data/0008000.bop';
-fo  = 'o.vtk';
+% usage: bop2vtk.m output.file input.file
+err   = @(varargin) fprintf(stderr(), varargin{:});
+usage = @() err('usage: bop2vtk.m output.file input.file');
 
-fd = fopen(fn);
-n = fread(fd, 1, 'int64');
+if (numel(argv()) != 2); usage(); exit(1); end
+fo = argv(){1}; % output
+fi = argv(){2}; % input
+
+fd = fopen(fi);
+if fd == -1; err('bop2vtk.m: cannot open %s', fi); exit(1); end
+n = fread(fd, 1, 'int64'); n = -10;
+if n < 0 || n > 1e8; err('bop2vtk.m: wrong n: %ld', n); exit(1); end
 D = fread(fd, 'float32');
 fclose(fd);
 D = reshape(D, [], n);
 
 p = @(fmt, varargin) fprintf(fd, [fmt, '\n'], varargin{:});
 fd = fopen(fo, 'w');
+if fd == -1; err('bop2vtk.m: cannot open %s', fo); exit(1); end
 p('# vtk DataFile Version 2.0');
 p('Created with bop2vtk.m');
 p('BINARY');
