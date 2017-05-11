84082cd1b1f7aa255a6e843f2705c8dec427c824
Sergey Litvinov <slitvinov@gmail.com>
2017-05-11 17:19:54 +0200

    Remove vtk dump from bop2grid

--- poc/bop2grid.cpp
+++ poc/bop2grid.cpp
@@ -17,76 +17,6 @@ float FloatSwap(float f) {
   return dat2.f;
 }
 
-namespace vtk
-{
-    float *vv, *ff;
-    
-    template <typename T>
-    void init(const long n, const int nvars, const T *data)
-    {
-        if (nvars < 3)
-        {
-            fprintf(stderr, "Need at least 3 coordinates x y z\n");
-            exit(1);
-        }
-
-        const int nf = nvars - 3;
-
-        ff = vv = NULL;
-        
-        if (nf > 0)
-        ff = new float[nf * n];
-        vv = new float[3  * n];
-        
-        for (long i = 0; i < n; ++i)
-        {
-            for (int d = 0; d < 3; ++d)
-            vv[3*i + d] = (float) data[nvars*i + d];
-
-            for (int d = 0; d < nf; ++d)
-            ff[n*d + i] = (float) data[nvars*i + 3 + d];
-        }
-
-        for (long i = 0; i < 3  * n; ++i) vv[i] = FloatSwap(vv[i]);
-        for (long i = 0; i < nf * n; ++i) ff[i] = FloatSwap(ff[i]);
-    }
-
-    void finalize()
-    {
-        if (vv) delete[] vv;
-        if (ff) delete[] ff;
-    }
-    
-    void header(FILE *f, const long n)
-    {
-        fprintf(f, "# vtk DataFile Version 2.0\n");
-        fprintf(f, "created with bop2grid\n");
-        fprintf(f, "BINARY\n");
-    }
-
-    void vertices(FILE *f, const long n)
-    {
-        fprintf(f, "DATASET POLYDATA\n");
-        fprintf(f, "POINTS %ld float\n", n);
-        fwrite(vv, 3*n, sizeof(float), f);
-        fprintf(f, "\n");
-    }
-
-    void fields(FILE *f, const long n, const int nvars, const Cbuf *vars)
-    {
-        if (nvars <= 3) return;
-
-        fprintf(f, "POINT_DATA %ld\n", n);
-
-        for (int i = 3; i < nvars; ++i)
-        {
-            fprintf(f, "SCALARS %s float\n", vars[i].c);
-            fprintf(f, "LOOKUP_TABLE default\n");
-            fwrite(ff + (i-3)*n, n, sizeof(float), f);
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
     if (argc < 3)
@@ -108,26 +38,6 @@ int main(int argc, char **argv)
     concatenate(nd, dd, /**/ &d);
 
     summary(&d);
-        
-    FILE *f = fopen(argv[1], "w");
-    
-    switch (d.type)
-    {
-    case FLOAT:
-    case ASCII:
-        vtk::init(d.n, d.nvars, d.fdata);
-        break;
-    case DOUBLE:
-        vtk::init(d.n, d.nvars, d.ddata);
-        break;
-    };
-
-    vtk::header  (f, d.n);
-    vtk::vertices(f, d.n);
-    vtk::fields  (f, d.n, d.nvars, d.vars);
-    vtk::finalize();
-
-    fclose(f);
 
     for (int i = 0; i < nd; ++i)
     finalize(dd + i);
