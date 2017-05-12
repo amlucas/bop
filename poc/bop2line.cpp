8781dacb80fac6d23be985cf32185a2f9f24c156
Sergey Litvinov <slitvinov@gmail.com>
2017-05-12 18:14:01 +0200

    Remove part related to 2D

--- poc/bop2line.cpp
+++ poc/bop2line.cpp
@@ -1,42 +1,21 @@
 #include <cstdlib>
 #include <cstdio>
-
 #include "reader.h"
-#include "grid2vtk.h"
 
-float FloatSwap(float f) {
-  union {
-    float f;
-    unsigned char b[4];
-  } dat1, dat2;
-
-  dat1.f = f;
-  dat2.b[0] = dat1.b[3];
-  dat2.b[1] = dat1.b[2];
-  dat2.b[2] = dat1.b[1];
-  dat2.b[3] = dat1.b[0];
-  return dat2.f;
-}
-
-int nx, ny;
-double ox, oy;
-double sx, sy;
+int n;    /* number of steps */
+double o; /* origin */
+double s; /* spacing */
 
 char *fo; /* output file */
 
 int main(int argc, char **argv) {
   int iarg = 1;
-  /* fprintf(stderr, "usage: %s <out.vtk> <nx> <ny> <ox> <oy> <sx> <sy> <in1.bop> <in2.bop> ...\n", argv[iarg++]); */
+  /* fprintf(stderr, "usage: %s <out.vtk> <n> <o> <s>  <in1.bop> <in2.bop> ...\n", argv[iarg++]); */
   fo =      argv[iarg++];
 
-  nx = atoi(argv[iarg++]);
-  ny = atoi(argv[iarg++]);
-
-  ox = atof(argv[iarg++]);
-  oy = atof(argv[iarg++]);
-
-  sx = atof(argv[iarg++]);
-  sy = atof(argv[iarg++]);
+  n = atoi(argv[iarg++]);
+  o = atof(argv[iarg++]);
+  s = atof(argv[iarg++]);
 
   const int nd = argc - iarg;
   printf("nd: %d\n", nd);
@@ -52,22 +31,7 @@ int main(int argc, char **argv) {
   concatenate(nd, dd, /**/ &d);
   summary(&d);
 
-  long     n[] = {nx, ny, 1};
-  double org[] = {ox, oy, 0};
-  double sp[]  = {sx, sy, 0};
-  double *vy, *vx;
-  char   const *nname[] = {"vx", "vy"};
-  int    ns = sizeof nname / sizeof nname[0];
-
-  long np = nx * ny;
-  vx = (double*)malloc(np*sizeof vx[0]);
-  vy = (double*)malloc(np*sizeof vy[0]);
-  for (long i = 0; i < np; i++) vx[i] = -i;
-  for (long i = 0; i < np; i++) vy[i] =  i;
-  double *vars[] = {vx, vy};
-  grid2vtk(fo,
-	   n, org, sp,
-	   vars, nname, ns);
+  
 
   for (int i = 0; i < nd; ++i) finalize(dd + i);
   finalize(&d);
