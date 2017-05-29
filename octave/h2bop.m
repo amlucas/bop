6c13f769e94c8958a46564026db23117ccafa202
Sergey Litvinov <slitvinov@gmail.com>
2017-05-29 15:24:00 +0200

    Make in and out parameters

--- octave/h2bop.m
+++ octave/h2bop.m
@@ -1,10 +1,14 @@
 #!/usr/bin/env octave-qf
 
-# To vtk
-# (cd out; for f in *.bop; do b=`basename $f .bop`; ../../bop2vtk $b.vtk $f; done)
+###### To vtk
+# ./h2bop.m out /tmp/run_122/h5/s.h5part
+# (cd out; for f in *.bop; do b=`basename $f .bop`; bop2vtk $b.vtk $f; done)
 
-f = "/tmp/run_122/h5/s.h5part";
-d = "out";
+d = argv(){1}; # output dir
+f = argv(){2}; # h5part file
+fmt = "%04d";
+
+function mkdir(d); c = sprintf("mkdir -p -- '%s'", d); system(c); endfunction
 
 function ffi = fsort(ffi) % sort fieldnames
   nf = numel(ffi);
@@ -21,16 +25,16 @@ DD = load(f);
 ffi = fieldnames(DD);
 ffi = fsort(ffi);
 
-
+mkdir(d);
 for i=1:numel(ffi); D = DD.(ffi{i});
     n = numel(D.x);
     l = "x y z vx vy vz type";
     D = [D.x; D.y; D.z; D.u; D.v; D.w; single(D.type)];
 
-    b = sprintf("%04d", i)
-    bop  = sprintf("%s/%s.bop", d, b)
-    val0 = sprintf(   "%s.values", b)
-    val  = sprintf("%s/%s.values", d, b)
+    b = sprintf(fmt, i);
+    bop  = sprintf("%s/%s.bop", d, b);
+    val0 = sprintf(   "%s.values", b);
+    val  = sprintf("%s/%s.values", d, b);
 
     f = fopen(bop, "w");
     fprintf(f, "%ld\n", n);
