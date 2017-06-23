8ef0b091273ab88e2af21f53a8b784d2338b10ce
Sergey Litvinov <slitvinov@gmail.com>
2017-06-23 20:41:56 +0200

    add bop_write

--- octave/r.m
+++ octave/r.m
@@ -1,18 +1,9 @@
 #!/usr/bin/env octave-qf
 
+global e_c e_m
 system("make >/dev/null");
+e_c = 0;
 pkg load bop
+B = bop_read("test_data/1000.bop");
 
-n =  [80     1    80];
-s =  [1/20    4    1/20];
-o =  [-2    -2    -2];
-l = {"vx", "vy", "vz"};
-G = bop_igrid(l, n, o, s);
-
-l = glob("d/*.bop"); nl = numel(l);
-for i=100:nl; i = l{i};
-    B = bop_read(i);
-    G = bop_proj(G, B);
-endfor
-
-bop_grid(G.D, G.n, G.o, G.s, "o.vtk");
+bop_write(B, "./123.bop");
