b23e981190440ce5d5af9c3e1849530cb149b001
Sergey Litvinov <slitvinov@gmail.com>
2017-06-23 12:52:52 +0200

    upd: bop_grid

--- octave/inst/bop_grid.m
+++ octave/inst/bop_grid.m
@@ -1,28 +1,57 @@
-## write gird data to vtk : node centered
 function bop_grid(D, n, o, s, fn)
   ## D: structure with data : ex: D.vx(1:n(X), 1:n(Y), 1:n(Z))
   ## n(1:3): size, o(1:3) : origin, s(1:3) : spacing
-
+  ## fn: file name
   global e_c e_m # error code and message
   f = fopen(fn, "w");
+  if f == -1
+    e_m = sprintf("cannot write to %s", fn);
+    e_c = 1;
+    return
+  endif
+  grid(D, n, o, s, f)
+  fclose(f);
+endfunction
 
+## write gird data to vtk : node centered
+function grid(D, n, o, s, f)
+  global e_c e_m # error code and message
 
   if !isstruct(D) ; e_m = "not a structure"; e_c = 1; return; endif
   if numel(n) != 3; e_m = "wrong n"        ; e_c = 1; return; endif
   if numel(o) != 3; e_m = "wrong o"        ; e_c = 1; return; endif
   if numel(s) != 3; e_m = "wrong s"        ; e_c = 1; return; endif
-  if f == -1      ; e_m = sprintf("cannot write to %s", fn); e_c = 1; return; endif
 
   header(f)
   topology(f, n, o, s);
-  fclose(f);
+
+  l = fieldnames(D);
+  nv = numel(l);
+  if nv == 0; return; endif
+  data_header(f, n)
+
+  for i=1:nv; i = l{i}; scalar(f, D.(i), n, i); endfor
+endfunction
+
+function scalar(f, D, n, name)
+  X = 1; Y = 2; Z = 3;
+  nn = n(X)*n(Y)*n(Z);
+  fprintf(f, "SCALARS %s double\n", name)
+  fprintf(f, "LOOKUP_TABLE default\n")
+  dlmwrite(f, D, '\n'); # change if BINARY
+endfunction
+
+function data_header(f, n)
+  X = 1; Y = 2; Z = 3;
+  nn = n(X)*n(Y)*n(Z);
+  fprintf(f, "POINT_DATA %ld\n", nn);
 endfunction
 
 function header(f)
   fprintf(f, "# vtk DataFile Version 2.0\n")
-  fprintf(f, "created with bop2grid\n")
+  fprintf(f, "created with bop_grid\n")
   fprintf(f, "ASCII\n")
-  #   pr("BINARY\n");
+  #pr("BINARY\n");
 endfunction
 
 function topology(f, n, o, s)
