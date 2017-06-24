212745434ab95a53a65158b75bcd4f898385aeae
Sergey Litvinov <slitvinov@gmail.com>
2017-06-24 19:33:57 +0200

    A plan for bop_p2p

--- octave/r2.m
+++ octave/r2.m
@@ -4,5 +4,13 @@ system("make > /dev/null");
 pkg load bop
 global e_m e_c
 
-[B, F] = bop_read_off("test_data/sph.162.off");
-bop_write_tri(B, F, "t.vtk");
+[Bs, Fs] = bop_read_off("test_data/sph.162.off");
+B        = bop_read("test_data/1000.bop");
+
+bop_write_tri(Bs, Fs, "s.vtk");
+bop_write_vtk( B,     "b.vtk");
+
+Bs = bop_p2p_accum(Bs, B, {"vx", "vy", "vz"}); # accumulate
+Bs = bop_p2p_norm (Bs);                        # normalize by "density"
+
+e_m
