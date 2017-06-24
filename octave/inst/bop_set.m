0632fd5fea7ed7fb0730c08d29c1fd54fce0b4c8
Sergey Litvinov <slitvinov@gmail.com>
2017-06-24 12:09:21 +0200

    add iset; make set work with varargin

--- octave/inst/bop_set.m
+++ octave/inst/bop_set.m
@@ -1,5 +1,13 @@
 # set default argument list for bop_pop
-function bop_set(arg)
+function bop_set(varargin)
+  if nargin == 1 && iscell(fst = varargin{1})
+    set0(fst)
+  else
+    set0(varargin)
+  endif
+endfunction
+
+function set0(arg)
   global darg iarg
   darg = arg; iarg = 1;
 endfunction
