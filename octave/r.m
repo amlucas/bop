d124bfef2ba0afd34801936dbf424065d3911f2f
Sergey Litvinov <slitvinov@gmail.com>
2017-06-24 13:51:54 +0200

    clean up

--- octave/r.m
+++ octave/r.m
@@ -2,7 +2,6 @@
 
 pkg load bop
 global e_c e_m; e_c = 0;
-
 X = 1; Y = 2; Z = 3;
 
 function [x, y, z, vx, vy, vz] = read()
@@ -18,42 +17,35 @@ function [x, y, z, vx, vy, vz] = read()
   vx = B.vx; vy = B.vy; vz = B.vz;
 endfunction
 
-# interactive?
-function r = iact()
-  eq = @bop_eq;
-  r = numel(a = argv()) >= 1 && eq(a{1}, "-i");
+function [x, y, z] = rot(x, y, z, R)
+  X = 1; Y = 2; Z = 3;
+  n = numel(x);
+  for i=1:n
+    r = [x(i) y(i) z(i)];
+    r = r * R;
+    x(i) = r(X); y(i) = r(Y); z(i) = r(Z);
+  endfor
 endfunction
 
-if iact(); bop_set(glob("~/s/sh_3.0/ply/rbcs-01*.ply")); endif
+function [f, E, vx0, vz0] = sk_fit(x, z, vx, vz, q)
+  # q: ax/az; E: sq. error; v[xz]0: prediction
+  vxz = sum(vx.*z); vzx = sum(vz.*x);
+  xx  = sum(x.*x);   zz = sum(z.*z);
+  q2 = q^2; q4 = q^4;
+  f = (q*(q2*vxz-vzx))/(q4*zz+xx);
+
+  vx0 = f *     q  * z;
+  vz0 = f * (-1/q) * x;
+  E  = (vx0 - vx).^2 + (vz0 - vz).^2;
+endfunction
+
+## fake command line arguments for interactive session
+bop_iset(glob("~/s/sh_3.0/ply/rbcs-01*.ply"))
 
 [x, y, z, vx, vy, vz] = read();
-[center, radii, R, v, chi2]  = efit([x', y', z']);
-ax = radii(X); ay = radii(Y); az = radii(Z);
-
-n = numel(x);
-for i=1:n
-  v = [vx(i) vy(i) vz(i)];
-  v = v * R;
-  vx(i) = v(X); vy(i) = v(Y); vz(i) = v(Z);
-endfor
-
-for i=1:n
-  r = [x(i) y(i) z(i)];
-  r = r * R;
-  x(i) = r(X); y(i) = r(Y); z(i) = r(Z);
-endfor
-clear r
-
-a = radii(X); b = radii(Z); ab = a/b;
-vxz = sum(vx.*z); vzx = sum(vz.*x);
-xx = sum(x.*x);    zz = sum(z.*z);
-ab2 = ab^2; ab4 = ab^4;
-f = (ab*(ab2*vxz-vzx))/(ab4*zz+xx);
-
+[center, a, R, v, chi2]  = efit([x', y', z']);
+[ x,  y,  z] = rot( x,  y,  z, R);
+[vx, vy, vz] = rot(vx, vy, vz, R);
+[f, E] = sk_fit(x, z, vx, vz, a(X)/a(Z));
+	
 printf("%g\n", f);
-
-vx0 = f *     ab  * z;
-vz0 = f * (-1/ab) * x;
-
-E  = (vx0 - vx).^2 + (vz0 - vz).^2;
-E0 = vx.^2 + vz.^2;
