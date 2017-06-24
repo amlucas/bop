28fb079ca565df20543e4f98c4343e904bb7c792
Sergey Litvinov <slitvinov@gmail.com>
2017-06-24 08:59:25 +0200

    upd r.m

--- octave/r.m
+++ octave/r.m
@@ -1,10 +1,11 @@
 #!/usr/bin/env octave-qf
 
 1;
+function r = eq(a, b); r = (strcmp(a, b) == 1); endfunction
 function r = nxt()
-  global iarg
-  narg = numel(argv());
-  if iarg <= narg; r = argv(){iarg++}; else r = []; endif
+  global iarg arg
+  narg = numel(arg);
+  if iarg <= narg; r = arg{iarg++}; else r = []; endif
 endfunction
 
 function r = nxt_num()
@@ -12,16 +13,26 @@ function r = nxt_num()
   r = str2num(r);
 endfunction
 
-global e_c e_m iarg
-iarg = 1;
+function argp()
+  global iarg arg
+  iarg = 1;
+  arg = argv();
+  if eq(arg{1}, "-i"); arg = arg_default(); endif
+endfunction
+
+function a = arg_default()
+  a = {"/home/lisergey/s/sh_4.0/ply/rbcs-01240.ply", "/home/lisergey/s/sh_4.0/ply/rbcs-01230.ply"};
+endfunction
+
+global e_c e_m
+argp()
 
 system("make >/dev/null");
-e_c = 0;
 pkg load bop
+e_c = 0;
+
 X = 1; Y = 2; Z = 3;
-
 B = struct();
-
 while 1
   b = nxt();
   if isempty(b); break; endif
@@ -31,18 +42,28 @@ while 1
   break
 endwhile
 
-[center, radii, evecs, v, chi2]  = efit([B.x', B.y', B.z']);
+[center, radii, R, v, chi2]  = efit([B.x', B.y', B.z']);
+
+ax = radii(X); ay = radii(Y); az = radii(Z);
 
 x = B.x; y = B.y; z = B.z;
 vx = B.vx; vy = B.vy; vz = B.vz;
 
-a = radii(X); b = radii(Z); ab = a/b;
-## svxy, svyx = sm(v[:, X]*r[:, Y]), sm(v[:, Y]*r[:, X])
-## sxx,   syy = sm(r[:, X]*r[:, X]), sm(r[:, Y]*r[:, Y])
-##     ab2 = ab**2
-##     ab4 = ab**4
-##     fr = (ab*(ab2*svxy-svyx))/(ab4*syy+sxx)
+n = numel(x);
+for i=1:n
+  v = [vx(i) vy(i) vz(i)];
+  v = v * R;
+  vx(i) = v(X); vy(i) = v(Y); vz(i) = v(Z);
+endfor
 
+for i=1:n
+  r = [x(i) y(i) z(i)];
+  r = r * R;
+  x(i) = r(X); y(i) = r(Y); z(i) = r(Z);
+endfor
+clear r
+
+a = radii(X); b = radii(Z); ab = a/b;
 vxz = sum(vx.*z); vzx = sum(vz.*x);
 xx = sum(x.*x);    zz = sum(z.*z);
 ab2 = ab^2;
