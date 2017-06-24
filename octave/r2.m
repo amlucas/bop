f8414b175f0387454d35c6f22d5c5a994861b26b
Sergey Litvinov <slitvinov@gmail.com>
2017-06-24 14:58:12 +0200

    add bop_write_vtk

--- octave/r2.m
+++ octave/r2.m
@@ -2,11 +2,12 @@
 
 system("make > /dev/null");
 pkg load bop
+global e_m e_c
 
-s = bop_pop()
-s = bop_pop()
-bop_push()
-s = bop_pop()
-s = bop_pop()
-s = bop_pop()
+X = 1; Y = 2; Z = 3;
+B.x = [1 2 3];
+B.y = [1 2 3];
+B.z = [1 2 3];
+B.vx = [1000, 2000, 3000];
 
+bop_write_vtk(B, "b.vtk");
