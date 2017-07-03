45a2b949fb402e9825d72bd9afd905db4f8f2d1c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-03 16:45:18 +0200

    clean reader

--- bop/bop_reader.h
+++ bop/bop_reader.h
@@ -1,27 +1,8 @@
 #ifndef BOP_READER_H
 #define BOP_READER_H
 
-#define CBUFSIZE 256
+#include "bop_common.h"
 
-struct Cbuf {char c[CBUFSIZE];};
-enum Type {FLOAT, DOUBLE, INT, ASCII};
-
-struct BopData {
-    long n;
-
-    int nvars;
-    Cbuf *vars;
-    Type type;
-    
-    float  *fdata;
-    double *ddata;
-    int    *idata;
-};
-
-void init(BopData *d);
-void finalize(BopData *d);
 void read(const char *fnbop, BopData *d);
-void summary(const BopData *d);
-void concatenate(const int nd, const BopData *dd, BopData *dall);
 
 #endif // BOP_READER_H
