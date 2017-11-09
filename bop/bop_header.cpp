247ac313b03ff25e6196ace3dc80c37f49db9e6a
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-09 15:18:40 +0100

    make it compile

--- bop/bop_header.cpp
+++ bop/bop_header.cpp
@@ -3,6 +3,7 @@
 
 #include "bop_common.h"
 #include "bop_utils.h"
+#include "bop_macros.h"
 #include "bop_header.h"
 
 static int is_desc(const char *s, const char *d) {
@@ -16,7 +17,7 @@ static void read_data_file(const char *data, /**/ char *df) {
 }
 
 static void read_variables(const char *data, /**/ BopData *d) {
-    strcpy(d->var, data);
+    strcpy(d->vars, data);
 }
 
 static void read_type(const char *data, /**/ BopData *d) {
@@ -36,7 +37,7 @@ static void extract_desc_data(const char *l, /**/ char *desc, char *data) {
 }
 
 static void parse_line(const char *l, /**/ char *dfname, BopData *d) {
-    char desc[NC] = {0}, data[NC] = {0};
+    char desc[CBUFSIZE] = {0}, data[CBUFSIZE] = {0};
     extract_desc_data(l, /**/ desc, data);
     
     if (is_desc(desc, "DATA_FILE"))
@@ -44,7 +45,7 @@ static void parse_line(const char *l, /**/ char *dfname, BopData *d) {
     else if (is_desc(desc, "DATA_FORMAT"))
         read_type(data, /**/ d);
     else if (is_desc(desc, "VARIABLES"))
-        return;
+        read_variables(data, /**/ d);
     else
         printf("unprocessed desc: <%s>, data: <%s>\n", desc, data);
 }
@@ -57,8 +58,9 @@ static void read_n_data(const char *data, /**/ BopData *d) {
 
 static int count_vars(const char *var) {
     int i;
-    char buf[NC];
+    char buf[CBUFSIZE];
 
+    i = 0;
     while (1 == sscanf(var, "%s", buf)) {
         var = strstr(var, buf);
         var += strlen(buf);
@@ -69,12 +71,12 @@ static int count_vars(const char *var) {
 
 void bop_read_header(const char *fname, /**/ char *dfname, BopData *d) {
     FILE *f;
-    char line[NC];
+    char line[CBUFSIZE];
     int l = 0;
 
     f = fopen(fname, "r");
     
-    while (EOF != fscanf(f, " %" xstr(NC) "[^\n]c", line)) {
+    while (EOF != fscanf(f, " %" xstr(CBUFSIZE) "[^\n]c", line)) {
         if (l == 0) read_n_data(line, /**/ d);
         else        parse_line(line, /**/ dfname, d);
         ++l;
