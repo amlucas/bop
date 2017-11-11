9736aa2eb2d170fab0b31a23b14c8ac087e211ef
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-11 14:49:33 +0100

    clean macros

--- src/macros.h
+++ src/macros.h
@@ -1,7 +1,2 @@
 #define xstr(s) str(s)
 #define str(s) #s
-
-#define ERR(...) do {                               \
-        fprintf(stderr,"(reader) " __VA_ARGS__);    \
-        exit(1);                                    \
-    } while(0)
