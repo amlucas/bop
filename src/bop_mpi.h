d021ec95be4c9f4c4c6c9cd71e0b29f882ef8b7e
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-03 17:45:41 +0100

    empty function

---
+++ src/bop_mpi.h
@@ -0,0 +1,5 @@
+BopStatus bop_write_header(const char *name, const BopData *d);
+BopStatus bop_write_values(const char *name, const BopData *d);
+
+BopStatus bop_read_header(const char *hfname, BopData *d, char *dfname);
+BopStatus bop_read_values(const char *dfname, BopData *d);
