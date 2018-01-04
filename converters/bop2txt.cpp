55c595c8dddd1c047095f7cceb15d91f520536aa
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-04 12:09:27 +0100

    option for summary in bop2txt

--- converters/bop2txt.cpp
+++ converters/bop2txt.cpp
@@ -1,8 +1,10 @@
-#include <cstdlib>
-#include <cstdio>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "bop_common.h"
 #include "bop_serial.h"
+#include "check.h"
 
 template <typename real>
 void float_print(const real *data, const long n, const int nvars) {
@@ -25,24 +27,34 @@ int main(int argc, char **argv) {
     char dfname[256];
     BopData *d;
     BopType type;
-    int i, nvars;
+    bool summary = false;
+    int istrt, i, nvars;
     long n;
     if (argc < 2) {
-        fprintf(stderr, "usage: %s <in1.bop> <in2.bop> ...\n", argv[0]);
+        fprintf(stderr, "usage: %s <OPT> <in1.bop> <in2.bop> ...\n", argv[0]);
+        fprintf(stderr, "\tOPT: -s for summary");
+        
         exit(1);
     }
 
-    for (i = 1; i < argc; ++i) {
+    istrt = 1;
+    if (strcmp(argv[1], "-s") == 0) {
+        ++istrt;
+        summary = true;
+    }
 
-        bop_ini(&d);
-        bop_read_header(argv[i], /**/ d, dfname);
-        bop_alloc(d);
-        bop_read_values(dfname, /**/ d);
-        // bop_summary(d);
+    for (i = istrt; i < argc; ++i) {
 
-        bop_get_type(d, &type);
-        bop_get_n(d, &n);
-        bop_get_nvars(d, &nvars);
+        BPC(bop_ini(&d));
+        BPC(bop_read_header(argv[i], /**/ d, dfname));
+        BPC(bop_alloc(d));
+        BPC(bop_read_values(dfname, /**/ d));
+        if (summary)
+            BPC(bop_summary(d));
+
+        BPC(bop_get_type(d, &type));
+        BPC(bop_get_n(d, &n));
+        BPC(bop_get_nvars(d, &nvars));
         
         switch (type) {
         case BopFLOAT:
@@ -57,7 +69,7 @@ int main(int argc, char **argv) {
             int_print((const int *) bop_get_data(d), n, nvars);
             break;
         };
-        bop_fin(d);
+        BPC(bop_fin(d));
     }    
     return 0;
 }
