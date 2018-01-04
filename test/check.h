2c163513d8025440beb62ed7b3e207cf072b2432
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-04 11:38:11 +0100

    fix check macro

--- test/check.h
+++ test/check.h
@@ -1,9 +1,9 @@
 #define BPC(ans) do {                           \
         BopStatus s = (ans);                    \
-        if (s != BOP_SUCCESS) {                 \
+        if (!bop_success(s)) {                  \
             fprintf(stderr, ":%s:%d: %s\n%s\n", \
                     __FILE__, __LINE__,         \
-                    bop_report_error_desc(s),   \
-                    bop_report_error_mesg());   \
+                    bob_report_error_desc(s),   \
+                    bob_report_error_mesg());   \
             exit(1);                            \
         }} while (0)
