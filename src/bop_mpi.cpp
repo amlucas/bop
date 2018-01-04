20050ab9f111ab362b8733667edeca3cb89b662a
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-04 10:50:57 +0100

    write header in mpi

--- src/bop_mpi.cpp
+++ src/bop_mpi.cpp
@@ -11,6 +11,9 @@
 
 #include "bop_mpi.h"
 
+using namespace bop_header;
+using namespace bop_utils;
+
 BopStatus bop_set_nrank(int n, BopData *d)  {
     if (d) {
         d->nrank = n;
@@ -42,7 +45,62 @@ BopStatus bop_get_nprank(const BopData *d, long *n)  {
     return BOP_NULLPTR;
 }
 
-BopStatus bop_write_header(MPI_Comm comm, const char *name, const BopData *d) {}
+template <typename T>
+static void write_mpi(MPI_Comm comm, const char *fname, long n, const T *data, MPI_Datatype type) {
+    MPI_Offset base, offset, len;
+    MPI_Status status;
+    MPI_File f;
+
+    MPI_File_open(comm, fname , MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &f);
+    MPI_File_set_size(f, 0);
+    MPI_File_get_position(f, &base);
+
+    len = n * sizeof(T);
+    offset = 0;
+    MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, comm);
+    MPI_File_write_at_all(f, base + offset, data, n, type, &status);
+    MPI_File_close(&f);
+}
+
+static BopStatus write_header(MPI_Comm comm, const char *fhname, const char *fdname, const BopData *d) {
+    int rank, n;
+    char *buf;
+    BopStatus s;
+    enum {MAX_SIZE = 2048};
+    MPI_Comm_rank(comm, &rank);
+
+    s = safe_malloc(MAX_SIZE * sizeof(char), (void**) &buf);
+    if (s != BOP_SUCCESS) return s;
+
+    n = 0;
+    if (rank == 0) {
+        n += sprintf(buf + n, "%ld\n", d->n);
+        n += sprintf(buf + n, "DATA_FILE: %s\n", fdname);
+        n += sprintf(buf + n, "DATA_FORMAT: %s\n", type2str(d->type));
+        n += sprintf(buf + n, "VARIABLES %s\n", d->vars);
+        n += sprintf(buf + n, "NRANK %ld\n", d->nrank);
+    }
+    n += sprintf(buf + n, "%ld\n", d->nprank);
+
+    write_mpi(comm, fhname, n, buf, MPI_CHAR);
+    
+    free(buf);    
+    return s;
+}
+
+BopStatus bop_write_header(MPI_Comm comm, const char *name, const BopData *d) {
+    char fnval[CBUFSIZE] = {0},
+        fnval0[CBUFSIZE] = {0},
+        fnhead[CBUFSIZE] = {0};
+
+    sprintf(fnhead, "%s.bop", name);
+
+    get_path(fnhead, fnval);
+    get_fname_values(fnhead, fnval0);
+    strcat(fnval, fnval0);
+
+    return write_header(comm, name, fnval0, d);
+}
 
 BopStatus bop_write_values(MPI_Comm comm, const char *name, const BopData *d) {}
 
