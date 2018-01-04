711c677d2e243ae893440721215352b3770cf8a9
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-04 11:38:38 +0100

    add dump write

--- test/write.cpp
+++ test/write.cpp
@@ -1,4 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bop_common.h"
+#include "bop_serial.h"
+#include "check.h"
+
 int main(int argc, char **argv) {
 
+    BopData *d;
+    int n;
+
+    n = 10;
+
+    BPC(bop_ini(&d));
+    BPC(bop_set_n(n, d));
+    BPC(bop_set_type(BopFASCII, d));
+    BPC(bop_set_vars(2, "x y", d));
+    BPC(bop_alloc(d));
+
+    BPC(bop_fin(d));
+    
     return 0;
 }
