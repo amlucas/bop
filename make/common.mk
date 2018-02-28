5c6578cfd30412c505caa0fb402a21bd7d3df755
Lucas Amoudruz (daint) <lucas.amoudruz@wanadoo.fr>
2018-02-28 13:08:16 +0100

    allow specific mpi flags

--- make/common.mk
+++ make/common.mk
@@ -1,6 +1,7 @@
 INST_BIN = $(HOME)/bin # where to install tools
 INST_LIB = $(HOME)/prefix/bop
 
-CXXFLAGS = -std=c++11 -Wpedantic -Wall -O3
-CXX=g++
-MPI_CXX=mpic++
+CXXFLAGS ?= -std=c++11 -Wpedantic -Wall -O3
+CXX      ?= g++
+MPI_CXX  ?= mpic++
+MPICXXFLAGS ?= $(CXXFLAGS)
--- make/rules.mk
+++ make/rules.mk
@@ -4,8 +4,8 @@ MPI_TOK=MPI_SPECIFIC_
 	$(CXX) $(CXXFLAGS) $(OPT) $< -c -o $@
 
 $(MPI_TOK)%.o: %.cpp
-	$(MPI_CXX) $(CXXFLAGS) $(OPT) -c $< -o $@
+	$(MPI_CXX) $(MPICXXFLAGS) $(OPT) -c $< -o $@
 
 %: %.o; $(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $^ $(LIBS)
 
-%: $(MPI_TOK)%.o; $(MPI_CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $^ $(LIBS_MPI)
+%: $(MPI_TOK)%.o; $(MPI_CXX) $(MPICXXFLAGS) $(LDFLAGS) -o $@ $^ $(LIBS_MPI)
