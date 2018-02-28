08a2aa8cd0f0a62d433f9aa9a8af39ca8511e826
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-28 11:39:15 +0100

    fix check

--- converters/check.h
+++ converters/check.h
@@ -3,7 +3,7 @@
         if (!bop_success(s)) {                  \
             fprintf(stderr, ":%s:%d: %s\n%s\n", \
                     __FILE__, __LINE__,         \
-                    bob_report_error_desc(s),   \
-                    bob_report_error_mesg());   \
+                    bop_report_error_desc(s),   \
+                    bop_report_error_mesg());   \
             exit(1);                            \
         }} while (0)
