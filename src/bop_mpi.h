542e2e5391502ee1acbf6812f85c622be75aa146
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-19 17:04:51 +0100

    add mpi doc

--- src/bop_mpi.h
+++ src/bop_mpi.h
@@ -1,5 +1,9 @@
-BopStatus bop_write_header(MPI_Comm comm, const char *name, const BopData *d);
-BopStatus bop_write_values(MPI_Comm comm, const char *name, const BopData *d);
+// tag::write[]
+BopStatus bop_write_header(MPI_Comm comm, const char *name, const BopData *d); // <1>
+BopStatus bop_write_values(MPI_Comm comm, const char *name, const BopData *d); // <2>
+// end::write[]
 
-BopStatus bop_read_header(MPI_Comm comm, const char *hfname, BopData *d, char *dfname);
-BopStatus bop_read_values(MPI_Comm comm, const char *dfname, BopData *d);
+// tag::read[]
+BopStatus bop_read_header(MPI_Comm comm, const char *hfname, BopData *d, char *dfname);  // <1>
+BopStatus bop_read_values(MPI_Comm comm, const char *dfname, BopData *d);                // <2>
+// end::read[]
