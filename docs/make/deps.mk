4a8a18849ba2b3c0e9c3a080cdbe5fa4eee38a53
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-20 09:30:08 +0100

    add tools to doc

--- docs/make/deps.mk
+++ docs/make/deps.mk
@@ -1,1 +1,1 @@
-index.html: index.adoc intro.adoc install.adoc interface.common.adoc interface.serial.adoc interface.mpi.adoc
+index.html: index.adoc intro.adoc install.adoc interface.common.adoc interface.serial.adoc interface.mpi.adoc tools.adoc
