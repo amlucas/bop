61914f47f5f499b389498b880377e0369213544c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-09 15:16:13 +0300

    add check macros for errors

--- python/pybop.cpp
+++ python/pybop.cpp
@@ -7,8 +7,15 @@
 
 using namespace std;
 
-// TODO
-#define BPC(ans) ans
+#define BPC(ans) do {                           \
+        BopStatus s = (ans);                    \
+        if (!bop_success(s)) {                  \
+            fprintf(stderr, ":%s:%d: %s\n%s\n", \
+                    __FILE__, __LINE__,         \
+                    bop_report_error_desc(s),   \
+                    bop_report_error_mesg());   \
+            exit(1);                            \
+        }} while (0)
 
 PyBop::PyBop()
     : d(nullptr)
